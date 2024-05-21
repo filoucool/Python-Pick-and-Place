7b1223e157fad7fdd16c559f7c0a9a23a2493b83
Félix Chenette-Stewart <felix@nexumnet.com>
2024-05-21 18:11:02 -0400

    Implement process_frame method to detect colored cubes
    Signed-off-by: Félix Chenette-Stewart <felix@nexumnet.com>

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# Commit message: Implement process_frame method to detect colored cubes
+
 import cv2
 import pyrealsense2 as rs
 import numpy as np
@@ -26,7 +28,24 @@ class RealSenseCubeDetector:
         }
 
     def process_frame(self, color_image):
-        pass
+        detected_cubes = []
+        blurred_image = cv2.GaussianBlur(color_image, (5, 5), 0)
+        hsv = cv2.cvtColor(blurred_image, cv2.COLOR_BGR2HSV)
+
+        for color, (lower, upper) in self.color_ranges.items():
+            mask = cv2.inRange(hsv, np.array(lower), np.array(upper))
+            mask = cv2.dilate(mask, self.kernel, iterations=1)
+            mask = cv2.erode(mask, self.kernel, iterations=1)
+            contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            for contour in contours:
+                if cv2.contourArea(contour) > self.min_area:
+                    x, y, w, h = cv2.boundingRect(contour)
+                    center = (x + w // 2, y + h // 2)
+                    detected_cubes.append({
+                        'color': color,
+                        'position': center
+                    })
+        return detected_cubes
 
     def detect_cubes(self):
         frames = self.pipeline.wait_for_frames()
